f2c357fe88d2744fd4dcba2c3a447e7ab03bdbec
vyshakh <vyshakh@dexlock.com>
2024-11-20 19:09:29 +0530

    redux-setup, cleanedup code, installed redux-thunk

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,12 @@ import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
+
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>
+  </Provider>
+  // <App />
 );
