846d87529348fc202fbe6d6f76afdf3d6f40574d
vyshakh <vyshakh@dexlock.com>
2025-01-07 11:34:56 +0530

    project setup done, with firebase setup

---
+++ src/config/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { Firestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD5KrERePRgCzUke5okPCMrmw4Rgg0GOK0",
+  authDomain: "file-management-system-13557.firebaseapp.com",
+  projectId: "file-management-system-13557",
+  storageBucket: "file-management-system-13557.firebasestorage.app",
+  messagingSenderId: "1021401648142",
+  appId: "1:1021401648142:web:39a9b5e7ea2d18c5e8805f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
